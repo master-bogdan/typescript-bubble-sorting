28f8a709be09a0f25759771d972df3fac1f10353
Bogdan <maiskiychavk@gmail.com>
2021-07-04 00:20:03 +0300

    [add linked list]

---
+++ build/LinkedList.js
@@ -0,0 +1,83 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LinkedList = void 0;
+var Node = /** @class */ (function () {
+    function Node(data) {
+        this.next = null;
+        this.data = data;
+    }
+    return Node;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.head = null;
+    }
+    LinkedList.prototype.add = function (number) {
+        var node = new Node(number);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        var tail = this.head;
+        while (tail.next) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    };
+    Object.defineProperty(LinkedList.prototype, "length", {
+        get: function () {
+            if (!this.head) {
+                return 0;
+            }
+            var length = 1;
+            var node = this.head;
+            while (node.next) {
+                length++;
+                node = node.next;
+            }
+            return length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    LinkedList.prototype.at = function (index) {
+        if (!this.head) {
+            throw new Error('Index is out of bounds');
+        }
+        var counter = 0;
+        var node = this.head;
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        throw new Error('Index is out of bounds');
+    };
+    LinkedList.prototype.compare = function (leftIndex, rightIndex) {
+        if (!this.head) {
+            throw new Error('List is empty');
+        }
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    };
+    LinkedList.prototype.swap = function (leftIndex, rightIndex) {
+        var leftNode = this.at(leftIndex);
+        var rightNode = this.at(rightIndex);
+        var leftHand = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    };
+    LinkedList.prototype.print = function () {
+        if (!this.head) {
+            return;
+        }
+        var node = this.head;
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    };
+    return LinkedList;
+}());
+exports.LinkedList = LinkedList;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([4, -3, 11, 2]);
-const charactersCollection = new CharactersCollection('cblora');
-
 const sorter = new Sorter(numbersCollection);
-const sorterString = new Sorter(charactersCollection);
-
 sorter.sort();
-sorterString.sort()
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('ZxYmba');
+const sorterString = new Sorter(charactersCollection);
+sorterString.sort()
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(-11);
+linkedList.add(-120);
+linkedList.add(240);
+linkedList.add(5);
+linkedList.add(401);
+const sorterLinkedList = new Sorter(linkedList);
+sorterLinkedList.sort();
+linkedList.print();
